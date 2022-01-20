7815a8d9984aadfa2adafab38666d3fd4a80d9c2
Antonin <antonin.info@protonmail.com>
2022-01-20 10:56:57 +0100

    Ajout script compil latex

--- latex/compil.sh
+++ latex/compil.sh
@@ -5,17 +5,33 @@
 # Usage : ./compil.sh nom_fichier.tex
 
 # Nom du rapport au format fichier.tex
-rapport_name=$1
+rapport_name="Rapport.tex"
 
 # Nom du rapport sans extension
 rapport_name_no_ext=$(echo ${rapport_name} | rev | cut -c5- | rev)
 
 xelatex ${rapport_name}
-xelatex ${rapport_name}
-bibtex ${rapport_name_no_ext}
-#biber ${rapport_name_no_ext}
-xelatex ${rapport_name}
-xelatex ${rapport_name}
+
+if [ "$?" == 0 ]
+then
+    xelatex ${rapport_name}
+    if [ "$?" == 0 ]
+    then
+        #bibtex $rapport_name_no_ext 
+        biber $rapport_name_no_ext
+        if [ "$?" == 0 ]
+        then
+            xelatex ${rapport_name}
+            xelatex ${rapport_name}
+        else
+            echo "Erreur de compilation des sources."
+        fi
+    else
+        echo "Erreur de compilation du fichier."
+    fi
+else
+    echo "Erreur de compilation du fichier."
+fi
 
 # Suppression des fichiers inutiles
-rm -f *.aux *.bbl *.toc *.log *.out *.run.xml *.bib *.blg
+rm -f *.aux *.bbl *.toc *.log *.out *.run.xml *.blg *blx.bib *.lof *.bcf
