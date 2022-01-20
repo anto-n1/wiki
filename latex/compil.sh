ddbca4db318fabdd869d31e7f8b39202253dae0f
Antonin <antonin.info@protonmail.com>
2022-01-20 10:54:41 +0100

    Ajout de memos pour LaTeX

--- latex/compil.sh
+++ latex/compil.sh
@@ -13,6 +13,7 @@ rapport_name_no_ext=$(echo ${rapport_name} | rev | cut -c5- | rev)
 xelatex ${rapport_name}
 xelatex ${rapport_name}
 bibtex ${rapport_name_no_ext}
+#biber ${rapport_name_no_ext}
 xelatex ${rapport_name}
 xelatex ${rapport_name}
 
